3aa3a03fd060faa519da87de685a8141a5f1ef2c
Neko <NicoNekoru@github.com>
2021-03-02 10:52:43 -0700

    Update ready

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-	module.exports = client => {
-		console.log(`Online in ${client.guilds.cache.size} servers.`);
-		client.user.setActivity('you deafen so I can add you to my hitlist', {type: 'WATCHING'})
-	}
+module.exports = client => {
+	console.log(`Online in ${client.guilds.cache.size} servers for ${client.users.cache.size} users on ws ${client.ws.gateway}.`);
+	client.user.setActivity('you deafen so I can add you to my hitlist', {type: 'WATCHING'})
+}
